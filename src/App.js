fe88acc9953042929218dcc0db89c29595e86daf
walterjrr <jrwalter731@gmail.com>
2023-11-19 18:24:47 -0300

    structure of project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" style={{ 
+    display: "flex", 
+    justifyContent: "center", 
+    border: "2.8px solid black", 
+    padding: "10px", 
+    margin: "0 300px",
+    borderRadius: "10px"
+}}>
+
+        <h1>PASSWORD GENERATOR</h1>
+        <input></input>
+        character fjbefbwqei
+  < input type = "range" ></input >
+        <div><input type="checkbox"></input> Uppercase?</div>
+        <div><input type="checkbox"></input> Lowercase?</div>
+        <div><input type="checkbox"></input> Numbers?</div>
+        <div><input type="checkbox"></input> Symbols?</div>
+        <div>
+          <h1 style={{border: "1px solid black"}}>Strength     </h1>
+        </div>
+    </div >
   );
 }
 
